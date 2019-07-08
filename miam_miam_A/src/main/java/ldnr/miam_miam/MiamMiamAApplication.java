27d37ef76a2d47ad84b3881ed5a24ce3b21d5cc6
stag <allagnat.matthieu@gmail.com>
2019-07-08 11:00:32 +0200

    First commit project creation

---
+++ miam_miam_A/src/main/java/ldnr/miam_miam/MiamMiamAApplication.java
@@ -0,0 +1,13 @@
+package ldnr.miam_miam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiamMiamAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiamMiamAApplication.class, args);
+	}
+
+}
---
+++ miam_miam_A/src/test/java/ldnr/miam_miam/MiamMiamAApplicationTests.java
@@ -0,0 +1,16 @@
+package ldnr.miam_miam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiamMiamAApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
