d1411951447f0449437d9982ecfe00e62c03f465
Matthieu Allagnat <allagnat.matthieu@gmail.com>
2019-07-09 11:18:43 +0200

    refactor boolean isDelivered

--- miam_miam_A/src/main/java/ldnr/miam_miam/model/Commande.java
+++ miam_miam_A/src/main/java/ldnr/miam_miam/model/Commande.java
@@ -10,49 +10,60 @@ public class Commande {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private Double price;
-	private Boolean deliveryState;
-	//@ManyToMany ?
+	private Boolean isDelivered;
+	// @ManyToMany ?
 	private List<Menu> menus;
 	private List<Pizza> pizzas;
 	private List<Boisson> boissons;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Double getPrice() {
 		return price;
 	}
+
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	public Boolean getDeliveryState() {
-		return deliveryState;
+
+	public Boolean getIsDelivered() {
+		return isDelivered;
 	}
-	public void setDeliveryState(Boolean deliveryState) {
-		this.deliveryState = deliveryState;
+
+	public void setIsDelivered(Boolean isDelivered) {
+		this.isDelivered = isDelivered;
 	}
+
 	public List<Menu> getMenus() {
 		return menus;
 	}
+
 	public void setMenus(List<Menu> menus) {
 		this.menus = menus;
 	}
+
 	public List<Pizza> getPizzas() {
 		return pizzas;
 	}
+
 	public void setPizzas(List<Pizza> pizzas) {
 		this.pizzas = pizzas;
 	}
+
 	public List<Boisson> getBoissons() {
 		return boissons;
 	}
+
 	public void setBoissons(List<Boisson> boissons) {
 		this.boissons = boissons;
 	}
-	
-	
+
 }
