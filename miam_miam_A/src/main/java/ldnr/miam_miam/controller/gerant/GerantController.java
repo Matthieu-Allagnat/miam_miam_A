a2f6f64862e2fcd57a660122383734bd3c8953fc
stag <stag@localhost>
2019-07-09 15:06:20 +0200

    commit

--- miam_miam_A/src/main/java/ldnr/miam_miam/controller/gerant/GerantController.java
+++ miam_miam_A/src/main/java/ldnr/miam_miam/controller/gerant/GerantController.java
@@ -1,10 +1,44 @@
 package ldnr.miam_miam.controller.gerant;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ldnr.miam_miam.model.Gerant;
+
+@Controller
 public class GerantController {
-	@RequestMapping("accueilGerant")
+	/*@RequestMapping("accueilGerant")
 	public String accueilGerant() {
 		return "Index de gestion";
-	}
+	}*/
+	
+	 private static List<Gerant> persons = new ArrayList<Gerant>();
+	 
+	    static {
+	        persons.add(new Gerant("Bill", "Gates"));
+	        persons.add(new Gerant("Steve", "Jobs"));
+	    }
+	 
+	    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	    public String index(Model model) {
+	 
+	        String message = "Hello Spring Boot + JSP";
+	 
+	        model.addAttribute("message", message);
+	 
+	        return "index";
+	    }
+	 
+	    @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
+	    public String viewPersonList(Model model) {
+	 
+	        model.addAttribute("persons", persons);
+	 
+	        return "personList";
+	    }
 }
--- miam_miam_A/src/main/java/ldnr/miam_miam/model/Gerant.java
+++ miam_miam_A/src/main/java/ldnr/miam_miam/model/Gerant.java
@@ -1,5 +1,33 @@
 package ldnr.miam_miam.model;
 
 public class Gerant {
+	
+	   private String firstName;
+	    private String lastName;
+	 
+	    public Gerant() {
+	 
+	    }
+	 
+	    public Gerant(String firstName, String lastName) {
+	        this.firstName = firstName;
+	        this.lastName = lastName;
+	    }
+	 
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	 
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+	 
+	    public String getLastName() {
+	        return lastName;
+	    }
+	 
+	    public void setLastName(String lastName) {
+	        this.lastName = lastName;
+	    }
 
 }
