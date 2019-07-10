526d53227435aaa735f2104d00da98745e929d98
stag <stag@localhost>
2019-07-10 15:30:47 +0200

    ajout page carte

---
+++ miam_miam_A/src/main/java/ldnr/miam_miam/controller/CarteController.java
@@ -0,0 +1,31 @@
+package ldnr.miam_miam.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ldnr.miam_miam.model.Pizza;
+
+@Controller
+public class CarteController {
+	
+	private static List<Pizza> pizzas = new ArrayList<Pizza>();
+	 
+    static {
+        pizzas.add(new Pizza("margarita", "s", 12 ));
+        pizzas.add(new Pizza("4 fromages", "xl", 15));
+        pizzas.add(new Pizza("buffalo", "m", 13));
+        pizzas.add(new Pizza("thon", "xl", 16));
+    }
+	
+	@RequestMapping(value = { "/carte" }, method = RequestMethod.GET)
+    public String index(Model model) {
+		model.addAttribute("pizzas", pizzas);
+ 
+        return "carte";
+    }
+}
--- miam_miam_A/src/main/java/ldnr/miam_miam/model/Pizza.java
+++ miam_miam_A/src/main/java/ldnr/miam_miam/model/Pizza.java
@@ -62,5 +62,45 @@ public class Pizza {
 		this.supplements = supplements;
 	}*/
 	
+
+    private String name;
+    private String size;
+    private int price;
+ 
+    public Pizza() {
+ 
+    }
+ 
+    public Pizza(String name, String size, int price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+ 
+   
 	
 }
