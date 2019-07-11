68585cb13548d5412269b4b73405a3483ae0b43b
Emmanuel <mamelune@gmail.com>
2019-07-11 18:24:04 +0200

    classe importées product venant de guru

---
+++ miam_miam_A/src/main/java/ldnr/miam_miam/dbbController/ProductRepository.java
@@ -0,0 +1,9 @@
+package ldnr.miam_miam.dbbController;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
---
+++ miam_miam_A/src/main/java/ldnr/miam_miam/dbbController/ProductService.java
@@ -0,0 +1,19 @@
+package ldnr.miam_miam.dbbController;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(Long id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(Long id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+}
