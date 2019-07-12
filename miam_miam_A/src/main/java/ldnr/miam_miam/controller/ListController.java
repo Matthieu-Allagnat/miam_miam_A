d5b1241dc857c42c7892bb2204f2098b7c086b3b
Emmanuel <mamelune@gmail.com>
2019-07-12 14:08:00 +0200

    ii

---
+++ miam_miam_A/src/main/java/ldnr/miam_miam/controller/ListController.java
@@ -0,0 +1,18 @@
+/*
+ * package ldnr.miam_miam.controller;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.ui.Model; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod;
+ * 
+ * @Controller public class ListController {
+ * 
+ * @RequestMapping(value= {"/list"}, method=RequestMethod.GET)
+ * 
+ * public String index(Model model) { return "list";
+ * 
+ * }
+ * 
+ * }
+ */
